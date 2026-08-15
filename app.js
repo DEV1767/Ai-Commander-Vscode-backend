70b86bf179d723dbb541f0105e3c2709b116a49d
shivam chaudhary <shivamchy076@gmail.com>
2026-08-15 19:17:55 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import authroute from "./src/routes/auth.router.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
 app.set("trust proxy", 1);
 app.use(cookieParser())
-
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.send("Server is running");
