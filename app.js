ab3a065ddc5781fc0cf4fa8f0bb5434ed4d80132
shivam chaudhary <shivamchy076@gmail.com>
2026-08-15 19:49:42 +0530

    Updated cors url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.set("trust proxy", 1);
 app.use(cookieParser())
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['https://ai-commander-frontend-topaz.vercel.app/'],
     credentials: true
 }));
 
